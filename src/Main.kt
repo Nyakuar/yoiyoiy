0f5cc6592f1e701a8ac7a8e360aacdab9aaa3058
Nyakuar <nyakuaryy@gmail.com>
2022-02-16 20:59:24 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main (){
+  var name = "nyakuar yiech"
+    println(name)
+  var phonenumber = "0721331910"
+    println(phonenumber)
+    val age = "23"
+    println(age)
+    val weight = "55"
+    println(weight)
+    var iskenyan:Boolean = true
+    println(iskenyan) // output true
+    var isnotkenyan:Boolean = false
+    println(isnotkenyan) // output false
+
+
+
+
+}
