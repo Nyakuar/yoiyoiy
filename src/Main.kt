a73cc7771fa3bf4288b461490e844c538fa0d900
Nyakuar <nyakuaryy@gmail.com>
2022-02-20 22:43:59 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,50 @@
 fun main (){
-  var name = "nyakuar yiech"
-    println(name)
-  var phonenumber = "0721331910"
-    println(phonenumber)
-    val age = "23"
-    println(age)
-    val weight = "55"
-    println(weight)
-    var iskenyan:Boolean = true
-    println(iskenyan) // output true
-    var isnotkenyan:Boolean = false
-    println(isnotkenyan) // output false
-
-
-
+  printName("Nyakuar")
+  var result = GetReminder(3,2)
+  println(result)
+  var sum= sum(4,8,3,2)
+  println(sum)
+  printinterestingfactaboutme("swimming")
 
 }
+
+
+
+
+
+
+
+fun printName(name:String){
+  println("Hello" + " " + name)
+}
+fun GetReminder(num1 : Int, num2: Int):Int{
+  var modulus =num1 % num2
+  return modulus
+
+}
+fun sum(a:Int,b:Int,c:Int,d:Int):Int{
+  var sum =a+b+c+d
+  return sum
+}
+
+fun printinterestingfactaboutme(fact:String){
+  println("I love" + " " +fact)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
